33e3977d68a85efbe98d8a253530bac1a579e807
qkrtjdgn <109455737+qkrtjdgn@users.noreply.github.com>
2022-07-17 17:44:10 +0900

    아날로그방식

--- digital_analog/digital_analog.ino
+++ digital_analog/digital_analog.ino
@@ -5,5 +5,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  while(1){
+    for(int i = 0; i<255; i++){
+      delay(1);
+      analogWrite(5,i);
+    }
+    for(int k = 255; k>0; k--){
+      delay(1);
+      analogWrite(5,k);
+     }
+  }
 }
