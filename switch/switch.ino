82539b39044b03c28f9a732fabc1c535aea90d67
qkrtjdgn <109455737+qkrtjdgn@users.noreply.github.com>
2022-07-17 18:52:50 +0900

    저항

--- switch/switch.ino
+++ switch/switch.ino
@@ -1,9 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMOde(
+  Serial.begin(9600);
+  pinMode(13, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(13)==HIGH){
+    Serial.println("1");
+  }
+  else if(digitalRead(13)==LOW){
+    Serial.println("0");
+  }
+  delay(10);
 }
